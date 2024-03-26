e05a2b9671610f85eeb19748ab57135cce5c7a0c
rdcmem <ric.costamoraes@gmail.com>
2024-03-26 00:59:33 -0300

    Atualização do Arquivo MovimentandoArquivos_v3 comentada

--- MovimentandoArquivos_v3.py
+++ MovimentandoArquivos_v3.py
@@ -5,27 +5,28 @@ import os
 #Essa funcao eu fiz para listar os arquivos do diretorio destino depois de mover o arquivo.
 def consulta_dir(endereco):
 	#Lista os arquivos do destino
-	x = -1
+	
+	#Inicia a contagem no -1 para a variável posicao seguir a indexacao que inicia em 0
+	x = -1 # 
+
+	#posição final da string que antecede o "\"
 	posicao = 0
 
 	#Passo pela string iterando o x para salvar a posicao do ultimo "\\"
 	for i in endereco :
-		#print(i)
 		x += 1
 		if(i=="\\"):
 			posicao = x
-			#letradaposicao = i
 	nova_string = endereco[0:posicao]
 	return nova_string
 
 #Recebe origem e destino
 origem = input('local de origem: ')
 destino = input('local de destino: ')
-#destino_consulta = input('local de destino para consulta: ')
 
 #Movimenta os arquivos
 os.rename(origem, destino)
 
-
+#Consulta o endereco de Destino para saber se o arquivo foi movido.
 resultado = os.listdir(consulta_dir(destino))
 print(resultado)
